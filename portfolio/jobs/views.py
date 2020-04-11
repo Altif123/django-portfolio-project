228f9b8509417799cce623e38edb5233a93e8759
Altif123 <altifali.aa@gmail.com>
2020-04-11 21:48:51 +0100

    Added home page with DB and static bootstap files

--- portfolio/jobs/views.py
+++ portfolio/jobs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Job
 
-# Create your views here.
+# when user selects home following function is executed 
+#passes the html and jobs model object
 def home(request):
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs':jobs})
