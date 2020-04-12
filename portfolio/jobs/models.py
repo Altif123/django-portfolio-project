939b5106bcaec4d8fe72962ebc94e2677f253c42
Altif123 <altifali.aa@gmail.com>
2020-04-12 14:21:41 +0100

    new nav bar template bootstrap

--- portfolio/jobs/models.py
+++ portfolio/jobs/models.py
@@ -6,4 +6,6 @@ class Job (models.Model):
  
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=300)
+    role = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
  
