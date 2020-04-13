55beb08dd014f6f448878e70cbbf2323f818e755
Altif123 <altifali.aa@gmail.com>
2020-04-13 20:16:29 +0100

    new model for projects

--- portfolio/jobs/models.py
+++ portfolio/jobs/models.py
@@ -3,9 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Job (models.Model):
- 
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=300)
+    summary = models.CharField(max_length=1000)
     role = models.CharField(max_length=100)
     company = models.CharField(max_length=100)
- 
+
+    def __str__(self):
+        return self.role
+
+class Project (models.Model):
+    where = models.CharField(max_length=300)
+    description = models.CharField(max_length=1000)
+    languages = models.CharField(max_length=300)
+    name = models.CharField(max_length=300)
+       
+    def __str__(self):
+        return self.name
--- portfolio/jobs/views.py
+++ portfolio/jobs/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
-from .models import Job
+from django.shortcuts import render, get_object_or_404
+from .models import Job,Project
 
 # when user selects home following function is executed 
 #passes the html and jobs model object
 def home(request):
     jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs':jobs})
+    projects = Project.objects
+    return render(request, 'jobs/home.html', {'jobs':jobs, 'projects':projects })
+
+def detail(request, job_id):
+    job_detail = get_object_or_404(Job, pk = job_id)
+    return render(request, 'jobs/detail.html', {'job':job_detail})
